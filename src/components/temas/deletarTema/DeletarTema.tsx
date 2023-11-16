1450884aaa72d65f52e2ee7f875b7c8504dd098a
Eduardo Nogueira <eduardo7_@hotmail.com>
2023-11-16 16:36:40 -0300

    atualização página perfil e personalização dos alertas com Toast

--- src/components/temas/deletarTema/DeletarTema.tsx
+++ src/components/temas/deletarTema/DeletarTema.tsx
@@ -6,6 +6,7 @@ import { AuthContext } from "../../../contexts/AuthContext";
 
 import Tema from "../../../models/Tema";
 import { RotatingLines } from "react-loader-spinner";
+import { toastAlerta } from "../../../utils/toastAlerta";
 function DeletarTema() {
 
     const navigate = useNavigate()
@@ -27,7 +28,7 @@ function DeletarTema() {
             })
         } catch (error: any) {
             if (error.toString().includes('403')) {
-                alert('O token expirou, favor logar novamente')
+                toastAlerta('O token expirou, favor logar novamente', 'info')
                 handleLogout()
             }
         }
@@ -35,7 +36,7 @@ function DeletarTema() {
 
     useEffect(() => {
         if (token === '') {
-            alert('Você precisa estar logado')
+            toastAlerta('Você precisa estar logado', 'info')
             navigate('/login')
         }
     }, [token])
@@ -56,10 +57,10 @@ function DeletarTema() {
                 }
             })
 
-            alert('Tema apagado com sucesso')
+            toastAlerta('Tema apagado com sucesso', 'sucesso')
 
         } catch (error) {
-            alert('Erro ao apagar o Tema')
+            toastAlerta('Erro ao apagar o Tema', 'erro')
         }
 
         setIsLoading(false)
